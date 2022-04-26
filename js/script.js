80ef22dc060986c246a82d545e768cd82b84d106
Fede180600 <fede180600@libero.it>
2022-04-26 14:26:16 +0200

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// [] Generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100.
+
+// [] Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+
+// [] I numeri nella lista delle bombe non possono essere duplicati.
+
+// [] In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+
+// [] La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+
+// [] Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
